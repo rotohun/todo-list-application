d8688b94c9fa41eb583c62f03908ed62ce5d751a
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-22 14:50:34 -0400

    added steps to complete the project and created functions for each feature of the project

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+
+//Add a new task.
+var addTask = function (argument) {
+	//When the button is pressed.
+		//Create a new list item with the text from newtask.
+			//input textbox
+			//label
+			//input (text)
+			//button.edit
+			//button.delete
+}
+
+
+//Edit an existing task.
+var editTask = function (argument) {
+	// When the button is pressed
+		//if the class of the parent is .editmode
+			//Switch for .editmode
+			//label text become the input's value
+		//else
+			//switch to editmode
+			//input value becomes the label's text 
+		//Toggle .editmode on the parent
+}
+
+//Delete an existing task.
+var deleteTask = function (argument) {
+	//When the delete button is pressed
+		//Remoxe the parent list item from the ul.
+}
+
+
+//Mark an existing task as complete.
+var markComplete = function (argument) {
+	//When the checkbox is checked 
+		//Appened the task list item to #completed-tasks
+}
+
+//Mark a task as incomplete.
+var markIncomplete = function (argument) {
+	//When the checkbox is unchecked
+		//append the task to the incomplete.	
+}
